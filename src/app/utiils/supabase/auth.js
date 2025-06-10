22502cc5b50f9d8b7359a86f32de185574664688
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-06-10 17:21:30 +0530

    implement signup functionality using Supabase authentication

---
+++ src/app/utiils/supabase/auth.js
@@ -0,0 +1,9 @@
+import supabase from "./client";
+
+export async function signUp(email, password) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  return { data, error };
+}
