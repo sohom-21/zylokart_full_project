a1ef7d7bb43316cb5b130e2419f4e2499ec2573c
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-06-10 06:23:33 +0530

    initialize Supabase client with environment variables

--- src/app/utiils/supabase/client.js
+++ src/app/utiils/supabase/client.js
@@ -1,9 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { configDotenv } from "dotenv";
-configDotenv();
+import {createClient} from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
