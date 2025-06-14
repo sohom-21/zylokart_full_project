2b51c9815669393219699ab2b2ef9c72700b57e0
Manas Raj <manasraj1357@gmail.com>
2025-06-14 11:56:18 +0530

    made loader.js

---
+++ src/app/components/loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const App = () => {
+  return (
+    <DotLottieReact
+      src="path/to/animation.lottie"
+      loop
+      autoplay
+    />
+  );
+};
