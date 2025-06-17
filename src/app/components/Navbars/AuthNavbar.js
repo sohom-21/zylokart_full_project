fdaf4cb751c7f088be03d7a16a3aa4700676ae9c
Sohom Ghosh <139136976+sohom-21@users.noreply.github.com>
2025-06-17 22:03:46 +0530

    fix(auth): correct import path for supabase client in AuthNavbar component

--- src/app/components/Navbars/AuthNavbar.js
+++ src/app/components/Navbars/AuthNavbar.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import NavbarLanding from './Navbar-landing';
 import NavbarCustomer from './navbar-customer';
-import supabase from '@/app/utiils/supabase/client';
+import supabase from '../../utiils/supabase/client';
 
 const AuthNavbar = () => {
   const [session, setSession] = useState(null);
